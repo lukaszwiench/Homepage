f08274b47bbb6449a7ccfa5325368fcdf334a05f
Łukasz Wiench <lukasz.wiench@wp.pl>
2020-10-16 18:31:05 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const name = 'Maciek';
+const age = 32;
+const firstName = 'Jacek';
+
+console.log(firstName);
+
+<!--KOMENTARZ-->
+<!--alert('siemanko');-->
+
+console.log('Siema!');
+
+console.log(`Siema, nazywam się ${firstName} i mam ${age} lat`);
