98c5475de2ef66921150cff47d1ea91ac3dcdf33
Łukasz Wiench <lukasz.wiench@wp.pl>
2020-11-18 00:22:06 +0100

    małe zmiany

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 const name = 'Maciek';
-const age = 32;
-const firstName = 'Jacek';
+const age = 19;
+const firstName = 'Łukasz';
 
 console.log(firstName);
 
-<!--KOMENTARZ-->
-<!--alert('siemanko');-->
+// KOMENTARZ-->
+// alert('siemanko')
 
 console.log('Siema!');
 
 console.log(`Siema, nazywam się ${firstName} i mam ${age} lat`);
+
+
+
+const heading = document.querySelector('.main-heading');
+
+console.log(heading);
+
+const namez = document.querySelector('.main-heading--js');
+
+namez.innerHTML = `Siema, nazywam się ${firstName} i mam ${age} lat`;
