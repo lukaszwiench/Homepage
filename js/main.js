d6b3dc8a64825a7bc432d62e11cb997bb22b6e61
Łukasz Wiench <lukasz.wiench@wp.pl>
2021-01-15 17:44:21 +0100

    buttons & and new look

--- js/main.js
+++ js/main.js
@@ -22,3 +22,20 @@ namez.innerHTML = `Siema, nazywam się ${firstName} i mam ${age} lat`;
 const git = document.querySelector(".js-git");
 
 git.innerHTML = `a tutaj mała zmiana potrzebna do wykonania zadania `;
+
+
+const button = document.querySelector('.action--js');
+console.log(button)
+
+button.addEventListener('click', () => {
+    const heading = document.querySelector(".main-heading--js");
+    heading.innerHTML = `Witaj Drogi odwiedzający, nazywam się Maciek`;
+    heading.classList.add(`klasa-z-js`);
+});
+
+const hamburger = document.querySelector('.hamburger--js');
+
+    hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+});
